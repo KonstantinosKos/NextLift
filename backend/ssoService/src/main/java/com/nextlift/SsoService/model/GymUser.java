1713a3183706a41bd3164978615a39e6eb54463a
bjnike <ahileas123@hotmail.com>
2025-02-23 11:22:07 +0000

    added Model code

--- backend/ssoService/src/main/java/com/nextlift/SsoService/model/GymUser.java
+++ backend/ssoService/src/main/java/com/nextlift/SsoService/model/GymUser.java
@@ -1,16 +1,30 @@
 package com.nextlift.SsoService.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class GymUser {
+// @Getter
+// @Setter
+// @AllArgsConstructor
+// @NoArgsConstructor
+// public class GymUser {
 
-    private int age;
-    private String name;
-}
+//     private int age;
+//     private String name;
+// }
+
+@Entity  // αυτή η κλάση εκπροσωπεί ένα table στο database του Χρηστάρα
+@Data  // Αυτόματη δημιουργία μεθόδων getter, setter, toString, κλπ.
+@NoArgsConstructor  // Δημιουργία κενού constructor
+@AllArgsConstructor  // Δημιουργία constructor με όλα τα πεδία
+@Builder  // Διευκόλυνση στη δημιουργία αντικειμένων
+public class User {
+    @Id  // Ορισμός του primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Αυτόματη δημιουργία μοναδικού ID
+    private Long id;  // Μοναδικό αναγνωριστικό χρήστη
+
+    private String username;  // Όνομα χρήστη
+    private String password;  // Κωδικός πρόσβασης
+}   
